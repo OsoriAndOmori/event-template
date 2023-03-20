aeb505a7223f1d3c46884747670e2d4eeb1e3c7f
김기훈 <skywhite15@sk.com>
2023-03-20 13:21:17 +0900

    바규니 결혼 초안

---
+++ main/resource/common/handlebars-helper.js
@@ -0,0 +1,8 @@
+Handlebars.registerHelper("math", function(lvalue, operator, rvalue, options) {
+    lvalue = parseFloat(lvalue);
+    rvalue = parseFloat(rvalue);
+
+    return {
+        "+": lvalue + rvalue
+    }[operator];
+});
